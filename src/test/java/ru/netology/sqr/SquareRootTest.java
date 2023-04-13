2e8a19eed391367e618776cfa092570e2abf4d1f
Dedurin <grem85krbr@mail,ru>
2023-04-13 20:58:06 +0400

    initial

--- src/test/java/ru/netology/sqr/SquareRootTest.java
+++ src/test/java/ru/netology/sqr/SquareRootTest.java
@@ -8,17 +8,17 @@ public class SquareRootTest {
 
     public void FindsСalcSQRT() {
         SquareRoot service = new SquareRoot();
-        int expected = 11;
-        int actual = service.calcSQRT(200, 300);
-        Assertions.assertEquals(expected, actual);
+        int quantity = 3;
+        int Range = service.calcSQRT(200, 300);
+        Assertions.assertEquals(Range, quantity);
     }
 
     @Test
 
     public void notFindСalcSQRT() {
         SquareRoot service = new SquareRoot();
-        int expected = 11;
-        int actual = service.calcSQRT(100, 400);
-        Assertions.assertEquals(expected, actual);
+        int quantity = 11;
+        int Range = service.calcSQRT(100, 400);
+        Assertions.assertEquals(Range, quantity);
     }
 }
